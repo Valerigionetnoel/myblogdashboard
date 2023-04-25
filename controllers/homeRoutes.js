77c90e42f82d1bc8ae7a1994763d2d69ce590875
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-25 19:39:24 -0300

    redid homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,30 +1,68 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, BlogPost } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const userData = await User.findAll({
-            attribute: { exclude: ['password']},
-            order: [['name', 'ASC']],
+        const blogPostData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                },
+            ],
         });
 
-        const users = userData.map((project) => project.get({ plain: true}));
+        const blogPosts = blogPostData.map((blogpost) => blogpost.get({plain: true}));
 
         res.render('homepage', {
-            users,
-            logged_in: req.session.logged_in,
+            blogPosts,
+            logged_in: req.session.logged_in
         });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(500).json(err)
     }
 });
 
-router.get('login', (req, res) => {
+router.get('/blogpost/:id', async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password']},
+            include: [{ model: BlogPost}],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-        res.redirect('/');
+        res.redirect('/profile');
         return;
     }
+    res.render('login')
 });
 
 module.exports = router;
