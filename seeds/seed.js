2734c1e244432ff5796b326b83dedd05e8e105d6
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-27 16:56:53 -0300

    change the seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, BlogPost, Comment } = require('../models');
+const { User, BlogPost,
+  //  Comment 
+  } = require('../models');
 
 const userData = require('./userData.json');
 const blogPostData = require('./blogPostData.json');
-const commentData = require('./commentData.json')
+// const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -20,12 +22,12 @@ const seedDatabase = async () => {
     });
   }
 
-  for (const comment of commentData) {
-    await Comment.create ({
-      ...comment,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const comment of commentData) {
+  //   await Comment.create ({
+  //     ...comment,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
