d3e083cf058d8a6e0fadb6d23235aaf953bd97c5
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-04 14:22:02 -0300

    almost done with this project

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,7 +2,7 @@ const NewFormHandler = async (event) => {
     event.preventDefault();
     const title = document.querySelector('#comment-title').value.trim();
     const description = document.querySelector('#comment-desc').value.trim();
-    const blogId = document.getElementById('blogId').value.trim();
+    const blogId = document.getElementById('blogId').value;
     // set the value of the blog id variable by getting the element by id
     console.log(title);
     console.log(description);
